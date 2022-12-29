163edb37df4a0abb062703f6b91ca6233822887d
Arpit078 <verma.arpit078@.com>
2022-12-29 15:20:37 +0530

    less goooo

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors({
-    origin:"http://127.0.0.1:5500",
+    // origin:"http://127.0.0.1:5500",
+    origin:"https://arpitverma.me",
     // origin:"http://localhost:5500"
   }))
 
