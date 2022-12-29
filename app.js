26a50cb38fb55183a9e8c5dd861eca79a8f16478
Arpit078 <verma.arpit078@.com>
2022-12-29 14:57:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+
+app.use(express.json())
+app.use(cors({
+    origin:"http://127.0.0.1:5500",
+    // origin:"http://localhost:5500"
+  }))
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,52 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+require("dotenv").config();
+const fs = require("fs")
+const app = require("./app")
+const random_number = Math.floor(Math.random() * 96)
+const PORT = 5000 || process.env.PORT
+
+let link =""
+
+fs.readFile("stories.json", function(err, data) {
+      
+    // Check for errors
+    if (err) throw err;
+   
+    // Converting to JSON
+    const story_url = JSON.parse(data);
+    //   console.log(story_url.links[random_number])
+     link = story_url.premchand[random_number].url// giving story url from a list of stories
+    //  console.log(story)
+
+    //less go with local variable here!
+
+app.get("/",(req,res)=>{
+    axios.get(link)
+    .then((response)=>{
+        const html = response.data
+        const $ = cheerio.load(html)//$(".itemListElement")
+
+        const story_title = $("h3.post-title").text()
+        const story_content = $("div.post-body").text()
+        // console.log(story_title,story_content)
+
+        res.json({
+            "heading":story_title,
+            "content":story_content
+           })    
+        })
+             
+    })
+           
+
+}) 
+
+
+
+
+
+
+app.listen(PORT,(req,res)=>{
+    console.log("connected!")
+})
