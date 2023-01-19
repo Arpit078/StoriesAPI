89ba4a3241a0fbd5e8a31937209e46860c7e752e
Arpit078 <verma.arpit078@.com>
2023-01-19 11:53:10 +0530

    keep alive!

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors({
-    // origin:"http://127.0.0.1:5500",
+    // origin:"http://localhost:19006",
     origin:"https://arpitverma.me",
     // origin:"http://localhost:5500"
   }))
--- index.js
+++ index.js
@@ -22,17 +22,17 @@ const cron = require('node-cron');
 const link_to_site = `https://stories-api.onrender.com/`
 
 
-// cron.schedule('0 */20 * * * *', () => {
+cron.schedule('0 */20 * * * *', () => {
 
-// axios.get(link_to_site, { 
-//     headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-// })
-//     .then((req,res) => {console.log(`ok`)})
-//     .catch((err)=>{
-//     // console.log(err)
-//     })
+axios.get(link_to_site, { 
+    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+})
+    .then((req,res) => {console.log(`ok`)})
+    .catch((err)=>{
+    // console.log(err)
+    })
 
-// });
+});
 
 
 
